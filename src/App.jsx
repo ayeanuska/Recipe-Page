adfc8f243da434ebabeaa6fcf4ac44f54576d3e7
Anuska <anuskasth2022@gmail.com>
2024-12-04 08:29:13 +1100

    search bar added to search food w their variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import RecipeGallery from "./RecipeGallery";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { RiStarSLine } from "react-icons/ri";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/RecipeGallery.jsx
+++ src/RecipeGallery.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import RecipeCard from "./RecipeCard";
 import RecipeViewer from "./RecipeViewer";
 
-const recipes = [
+const original_recipes = [
   {
     img: "https://www.amuserecipes.com/wp-content/uploads/2024/10/What-is-the-difference-between-Jhol-and-Momo-1.webp",
-    foodName: "M0 : M0",
-    intro: "Delicious nepalese dumplings",
+    foodName: "MO : MO",
+    intro: "Delicious Nepalese dumplings",
     descr:
       "Momo is a delicious and popular dumpling filled with a variety of savory fillings typically served with a spicy dipping sauce.",
     ingredients: [
@@ -49,7 +49,7 @@ const recipes = [
   {
     img: "https://www.kitchensanctuary.com/wp-content/uploads/2019/09/Spaghetti-Bolognese-tall-FS-0204.jpg",
     foodName: "Spagetti Bolognese",
-    intro: "Traditional itallian Spagetti bolognese",
+    intro: "Traditional Itallian Spagetti bolognese",
     descr:
       "Spaghetti Bolognese is a rich and flavorful pasta dish with ground beef, tomatoes, and a blend of aromatic herbs.",
     ingredients: ["Spaghetti", "Ground Beef", "Tomatoes", "Garlic", "Onions"],
@@ -200,17 +200,53 @@ const recipes = [
       "Thread chopped vegetables onto skewers, brush with olive oil, and sprinkle with garlic powder, salt, and pepper. Grill for 10-12 minutes, turning occasionally. Serve with a drizzle of lemon juice.",
   },
 ];
-const showRecipe = false;
+
 const RecipeGallery = () => {
+  const [recipes, setRecepies] = useState(original_recipes);
+
   const [showRecipe, setShowRecipe] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState({
     img: "",
     foodName: "",
     intro: "",
   });
+
+  const [searchVariable, setSearchVariable] = useState("");
+
+  const handleOnChange = (event) => {
+    setSearchVariable(event.target.value);
+  };
+
+  const searchTrigger = () => {
+    const filtered_recipe = original_recipes.filter((item) =>
+      item.foodName.toLowerCase().includes(searchVariable.toLowerCase())
+    );
+
+    setRecepies(filtered_recipe);
+  };
+
   return (
     <div>
       <h1>Recipe Gallery</h1>
+      <div className="searchbar">
+        <input
+          type="text"
+          name="search"
+          placeholder="search here"
+          value={searchVariable}
+          onChange={handleOnChange}
+        />
+
+        <button
+          onClick={() => {
+            alert(searchVariable);
+            searchTrigger();
+          }}
+        >
+          {" "}
+          search
+        </button>
+      </div>
 
       <div className="gallery-style">
         {recipes.map((item, index) => (
