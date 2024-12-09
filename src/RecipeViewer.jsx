42eae25032e08d57ba0c8859e0f5143efdfe96bc
Anuska <anuskasth2022@gmail.com>
2024-12-10 07:38:26 +1100

    bootstrap styling

--- src/RecipeViewer.jsx
+++ src/RecipeViewer.jsx
@@ -5,24 +5,36 @@ const RecipeViewer = ({ recipeData, CloseCard }) => {
     CloseCard(false);
   };
   return (
-    <div className="viewer-style ">
-      <img src={recipeData.img} />
-      <div className="details">
-        <h1>{recipeData.foodName}</h1>
-        <div>{recipeData.descr}</div>
-        <ul>
-          <h2> Ingredients</h2>
-          {recipeData.ingredients.map((item) => {
-            return <li>{item}</li>;
-          })}
-        </ul>
+    <div className="viewer-style d-flex flex-column align-items-center justify-content-center ">
+      <div container>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-8 col-lg-6" d-flex>
+            <img
+              src={recipeData.img}
+              className="img-fluid rounded position-absolute top-0 start-0 "
+            />
+            <div className="details rounded p-5">
+              <h2 className="text-center">{recipeData.foodName}</h2>
+              <p>{recipeData.descr}</p>
+              <ul>
+                <h2 className="text-center m-2"> Ingredients</h2>
+                {recipeData.ingredients.map((item) => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
 
-        <h2> Recipe </h2>
-        <div>{recipeData.recipe} </div>
-
-        <button onClick={CloseRecipeViewer} className="viewer-button">
-          Close
-        </button>
+              <h2 className="text-center m-2"> Recipe </h2>
+              <div>{recipeData.recipe} </div>
+              {/* className="viewer-button" */}
+              <button
+                className="btn btn-danger m-5 w-100 "
+                onClick={CloseRecipeViewer}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
